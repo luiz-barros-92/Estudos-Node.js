ab6e95039edb10c7fe4117b1c61f0d282b0e2866
Luiz Barros <lck.barros@gmail.com>
2025-02-03 21:45:03 -0300

    Implementação de view engine Ejs

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ import express from 'express';
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send("<html><body>Inicio</body></html>");
+    res.render("secao/inicio");
 })
 
 app.get('/projetos', function(req, res){
-    res.send("<html><body>Projetos</body></html");
+    res.render("secao/projetos");
 })
 
 app.get('/tutoriais', function(req, res){
-    res.send("<html><body>Tutoriais</body></html>");
+    res.render("secao/tutoriais");
 })
 
 app.get('/linux', function(req, res){
-    res.send("<html><body>Linux</body></html>");
+    res.render("secao/linux");
 })
 
 app.listen(3000, function(){
