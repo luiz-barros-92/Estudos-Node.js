760d3e6fae7889523be3e40d714a0655dbde7f4a
Luiz Barros <lck.barros@gmail.com>
2025-02-08 23:11:45 -0300

    Iniciando conexão com o banco de dados MySql para as páginas

--- app/routes/projetos.js
+++ app/routes/projetos.js
@@ -1,5 +1,19 @@
 export default function(app){
+
     app.get('/projetos', function(req, res){
-        res.render("projetos/projetos");
+    var mysql = require('mysql2');
+
+    var connection = mysql.createConnection({
+        host : '127.0.0.1',
+        user : 'root',
+        password : '1234',
+        database : 'luiz_barros'
+    });
+
+    connection.query('select * from projetos', function(error, result){
+        res.send(result);
+    });
+    
+       //res.render("projetos/projetos");
     });
 };
