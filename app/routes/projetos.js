94e6c34966a76ead986504f7b5f85603037a51ad
Luiz Barros <lck.barros@gmail.com>
2025-02-13 22:51:15 -0300

    Ajuste de renderização do arquivo Projetos

--- app/routes/projetos.js
+++ app/routes/projetos.js
@@ -1,19 +1,17 @@
 import mysql from 'mysql2';
 export default function(app){
 
-    app.get('/projetos', function(req, res){    
+    app.get('/projetos', function(req, res){
 
-    var connection = mysql.createConnection({
-        host : '127.0.0.1',
-        user : 'root',
-        password : '1234',
-        database : 'luiz_barros'
-    });
+        var connection = mysql.createConnection({
+            host : '127.0.0.1',
+            user : 'root',
+            password : '1234',
+            database : 'luiz_barros'
+        });
 
-    connection.query('select * from projetos', function(error, result){
-        res.send(result);
-    });
-    
-       //res.render("projetos/projetos");
+        connection.query('select * from projetos', function(error, result){
+            res.render("projetos/projetos", {projetos : result});
+        });
     });
 };
