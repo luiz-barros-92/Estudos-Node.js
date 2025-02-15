46e75b0c3c0587f8a656315dfad5e31df9fb58e8
Luiz Barros <lck.barros@gmail.com>
2025-02-14 21:35:22 -0300

    Conectando Inicio ao db

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,13 @@
+import dbConnection from '../../config/dbConnection.js';
+
 export default function(app){
+
+    var connection = dbConnection();
+
     app.get('/', function(req, res){
-        res.render("home/inicio");
+        
+        connection.query('select * from inicio', function(error, result){
+            res.render("home/inicio", {projetos : result});
+        });
     });
 };
