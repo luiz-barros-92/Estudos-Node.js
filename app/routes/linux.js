94e85b6cd084d9e55e10b4fef5d1514a570b3c69
Luiz Barros <lck.barros@gmail.com>
2025-02-14 21:37:27 -0300

    Conectando Linux ao db

--- app/routes/linux.js
+++ app/routes/linux.js
@@ -1,5 +1,13 @@
+import dbConnection from '../../config/dbConnection.js';
+
 export default function(app){
+
+    var connection = dbConnection();
+
     app.get('/linux', function(req, res){
-        res.render("linux/linux");
+
+        connection.query('select * from linux', function(error, result){
+            res.render("linux/linux", {projetos : result});
+        });
     });
 };
