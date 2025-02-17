9b66fd473b8523edf9a36dac7b908df359de4933
Luiz Barros <lck.barros@gmail.com>
2025-02-16 23:32:44 -0300

    Iniciando rotas com consign

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import app from './config/server.js';
 
-import rotaHome from './app/routes/home.js';
-rotaHome(app);
+//import rotaHome from './app/routes/home.js';
+//rotaHome(app);
 
-import rotaTutoriais from './app/routes/tutoriais.js';
-rotaTutoriais(app);
+//import rotaTutoriais from './app/routes/tutoriais.js';
+//rotaTutoriais(app);
 
-import rotaProjetos from './app/routes/projetos.js';
-rotaProjetos(app);
+//import rotaProjetos from './app/routes/projetos.js';
+//rotaProjetos(app);
 
-import rotaLinux from './app/routes/linux.js';
-rotaLinux(app);
+//import rotaLinux from './app/routes/linux.js';
+//rotaLinux(app);
 
 app.listen(3000, function(){
     console.log('Servidor Online!');
--- config/server.js
+++ config/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import consign from 'consign';
+
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+consign().include('app/routes').into(app);
+
 
 export default app;
